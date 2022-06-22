e0019f27bf5dbcd330942cc2922971ea6915c5a4
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-22 11:42:02 -0500

    fixed errors

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,54 +1,65 @@
-System;
-using System.Collections.Generic
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
-ProblematicProblem
+namespace ProblematicProblem
 {
-    Program class
+    class Program
     {
-        Random rng;
+        static Random rng=new Random();
 static bool cont = true;
-static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
         static void Main(string[] args)
 {
-    Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.Write("We are going to need your information first! What is your name? ");
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+            string s = Console.ReadLine();
+            if (!s.ToLower().Equals("yes")) return;
+            Console.WriteLine();
+            Console.Write("We are going to need your information first! What is your name? ");
     string userName = Console.ReadLine();
     Console.WriteLine();
     Console.Write("What is your age? ");
-    int userAge = Console.ReadLine();
+    int userAge = int.Parse(Console.ReadLine());
     Console.WriteLine();
     Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-    bool seeList = bool.Parse(Console.ReadLine());
+    bool seeList = Console.ReadLine().ToLower().Equals("sure");
     if (seeList)
     {
         foreach (string activity in activities)
         {
-            Console.Write($"{activity} ");
-            Thread.Sleep(250);
+                    //Console.WriteLine(activities.IndexOf(activity).GetType().Name);
+                    //Console.Write($"{activity} ");
+
+                    Console.Write($"{activity}" + (activities.IndexOf(activity).Equals(activities.Count - 1) ? "" : ", "));
+                    Thread.Sleep(250);
         }
         Console.WriteLine();
         Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-        bool addToList = bool.Parse(Console.ReadLine());
+        bool addToList = Console.ReadLine().ToLower().Equals("yes");
         Console.WriteLine();
-        while (addToList)
+                addToList:
+                while (addToList)
         {
             Console.Write("What would you like to add? ");
             string userAddition = Console.ReadLine();
+                    if (activities.Contains(userAddition))
+                    {
+                        Console.WriteLine("that's already in the list add something different");
+                        goto addToList;
+                    }
             activities.Add(userAddition);
-            foreach (string activity activities)
+            foreach (string activity in activities)
             {
-                Console.Write($"{activity} ");
+                //        Console.WriteLine(activities.IndexOf(activity).GetType().Name);
+                Console.Write($"{activity}"+(activities.IndexOf(activity).Equals(activities.Count-1)?"":", "));
                 Thread.Sleep(250);
             }
             Console.WriteLine();
             Console.WriteLine("Would you like to add more? yes/no: ");
-            string addToList = bool.Parse(Console.ReadLine());
+            addToList = Console.ReadLine().ToLower().Equals("yes");
         }
     }
-
+            cont = true;
     while (cont)
     {
         Console.Write("Connecting to the database");
@@ -57,28 +68,28 @@ static List<string> activities = new List<string>() { "Movies", "Paintball", "Bo
             Console.Write(". ");
             Thread.Sleep(500);
         }
-        Console.WriteLine()
+                Console.WriteLine();
                 Console.Write("Choosing your random activity");
         for (int i = 0; i < 9; i++)
         {
             Console.Write(". ");
             Thread.Sleep(500);
         }
-        Console.WriteLine()
+                Console.WriteLine();
                 int randomNumber = rng.Next(activities.Count);
-        string randomActivity = activities[randomNumber]
-                if (userAge > 21 && randomActivity == "Wine Tasting")
-        {
-            Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-            Console.WriteLine("Pick something else!");
-            activities.Remove(randomActivity);
-            string randomNumber = rng.Next(activities.Count);
-            string randomActivity = activities[randomNumber];
-        }
-        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
-        bool cont = bool.Parse(Console.ReadLine());
+                string randomActivity = activities[randomNumber];
+                while (userAge < 21 && randomActivity == "Wine Tasting")
+                {
+                    Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
+                    Console.WriteLine("Pick something else!");
+                    activities.Remove(randomActivity);
+                    randomNumber = rng.Next(activities.Count);
+                    randomActivity = activities[randomNumber];
+                }
+                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                cont = Console.ReadLine().ToLower().Equals("redo");
     }
 }
     }
-}}
+}
